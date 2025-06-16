5b0f58ca6b581def7032b365b58a95ca9e9c7ba5
EzraArya <ezrawijaya10@gmail.com>
2025-06-16 20:42:18 +0700

    feat: Implement WEBookmart + Add Additional Function

--- WebKitExperiment/Feature/ViewModel/WEWebViewModel.swift
+++ WebKitExperiment/Feature/ViewModel/WEWebViewModel.swift
@@ -10,25 +10,64 @@ import Combine
 import SwiftUI
 import WebKit
 
+protocol WEWebViewModelProtocol {
+    var bookmark: [WEBookmark] { get set }
+    var page: WebPage { get set }
+    
+    func addBookmark(title: String, url: URL)
+    func clearBookmark()
+    func loadPage(input: String)
+}
+
 @Observable
-final class WEWebViewModel {
-    var bookmark: [URL]
+final class WEWebViewModel: WEWebViewModelProtocol {
+    var bookmark: [WEBookmark]
     var page: WebPage
     
-    init(bookmark: [URL] = []) {
+    init(bookmark: [WEBookmark] = []) {
         self.bookmark = bookmark
         self.page = WebPage()
     }
     
-    func addBookmark(url: URL) {
-        bookmark.append(url)
+    func addBookmark(title: String, url: URL) {
+        let newBookmark = WEBookmark(url: url, title: title)
+        bookmark.append(newBookmark)
     }
     
     func clearBookmark() {
         bookmark.removeAll()
     }
     
-    func loadPage(url: URL) {
-        page.load(URLRequest(url: url))
+    func goBack() {
+        if let item = page.backForwardList.backList.last {
+            page.load(item)
+        }
+    }
+    
+    func goForward() {
+        if let item = page.backForwardList.forwardList.first {
+            page.load(item)
+        }
+    }
+    
+    func loadPage(input: String) {
+        var url: URL?
+        
+        if input.lowercased().starts(with: "http://") || input.lowercased().starts(with: "https://") {
+            url = URL(string: input)
+        } else if input.contains(".") && !input.contains(" ") {
+            url = URL(string: "https://" + input)
+        } else {
+            var components = URLComponents(string: "https://www.google.com/search")!
+            components.queryItems = [URLQueryItem(name: "q", value: input)]
+            url = components.url
+        }
+        
+        guard let validURL = url else {
+            print("Invalid URL or search term")
+            return
+        }
+        
+        page.load(URLRequest(url: validURL))
     }
 }
