5ff4541a66caa3514e3ecd64014bc089ad9b04be
EzraArya <ezrawijaya10@gmail.com>
2025-06-16 19:21:47 +0700

    feat: Add NavigationStack and VM environment on entry

--- WebKitExperiment/WebKitExperimentApp.swift
+++ WebKitExperiment/WebKitExperimentApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct WebKitExperimentApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ContentView()
+                    .environment(WebViewModel())
+            }
         }
     }
 }
