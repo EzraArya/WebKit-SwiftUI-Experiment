5492f4a6aea0d6f7b72e62cdb1136b5ddb932d48
EzraArya <ezrawijaya10@gmail.com>
2025-06-16 20:54:19 +0700

    feat: Add Empty State Handling + Implement WEBookmark

--- WebKitExperiment/Feature/View/WEBookmarkView.swift
+++ WebKitExperiment/Feature/View/WEBookmarkView.swift
@@ -10,27 +10,43 @@ import SwiftUI
 struct WEBookmarkView: View {
     @Environment(WEWebViewModel.self) private var viewModel
     @Environment(\.dismiss) private var dismiss
-    @Binding var bookmark: [URL]
+    @Binding var bookmarks: [WEBookmark]
     
-    init(bookmark: Binding<[URL]>) {
-        self._bookmark = bookmark
+    init(bookmarks: Binding<[WEBookmark]>) {
+        self._bookmarks = bookmarks
     }
     
     var body: some View {
-        List(bookmark, id: \.self) { url in
-            Text(String(url.absoluteString))
-                .onTapGesture {
-                    viewModel.loadPage(url: url)
-                    dismiss()
-                }
-        }
-        .navigationTitle("Bookmarks")
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                Button("Done") {
-                    dismiss()
+        NavigationStack {
+            VStack {
+                if bookmarks.isEmpty {
+                    ContentUnavailableView("No Bookmarks", systemImage: "bookmark.slash", description: Text("Add bookmarks using the '+' button."))
+                } else {
+                    List(bookmarks) { bookmark in
+                        VStack(alignment: .leading) {
+                            Text(bookmark.title)
+                                .fontWeight(.semibold)
+                            Text(bookmark.url.host() ?? "")
+                                .font(.caption)
+                                .foregroundStyle(.secondary)
+                        }
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            viewModel.loadPage(input: bookmark.url.absoluteString)
+                            dismiss()
+                        }
+                    }
                 }
             }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Done") {
+                        dismiss()
+                    }
+                }
+            }
+            .navigationTitle("Bookmarks")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
