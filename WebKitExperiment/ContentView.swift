e0e2bb66b084b743d65823d45be431187b17721e
EzraArya <ezrawijaya10@gmail.com>
2025-06-16 19:21:16 +0700

    feat: add MainView

--- WebKitExperiment/ContentView.swift
+++ WebKitExperiment/ContentView.swift
@@ -6,19 +6,90 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+    @Environment(WebViewModel.self) private var viewModel
+    @State private var showBookmark: Bool = false
+    
     var body: some View {
+        @Bindable var viewModel = viewModel
+
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            WebView(viewModel.page)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .webViewBackForwardNavigationGestures(.enabled)
+                .webViewElementFullscreenBehavior(.enabled)
+                .webViewLinkPreviews(.enabled)
         }
-        .padding()
+        .ignoresSafeArea(.all, edges: .bottom)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    guard let url = viewModel.page.url else { return }
+                    viewModel.addBookmark(url: url)
+                } label: {
+                    Image(systemName: "plus")
+                        .foregroundStyle(.black)
+                }
+            }
+            
+            ToolbarItem(placement: .principal) {
+                Text(viewModel.page.title)
+                    .font(.headline)
+                    .foregroundStyle(.black)
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    showBookmark.toggle()
+                } label: {
+                    Image(systemName: "bookmark")
+                        .foregroundStyle(.black)
+                }
+            }
+        }
+        .popover(isPresented: $showBookmark) {
+            BookmarkView(bookmark: $viewModel.bookmark)
+        }
+        .onAppear {
+            viewModel.loadPage(url: URL(string: "https://www.google.com")!)
+        }
     }
 }
 
+struct BookmarkView: View {
+    @Environment(WebViewModel.self) private var viewModel
+    @Environment(\.dismiss) private var dismiss
+    @Binding var bookmark: [URL]
+    
+    init(bookmark: Binding<[URL]>) {
+        self._bookmark = bookmark
+    }
+    
+    var body: some View {
+        List(bookmark, id: \.self) { url in
+            Text(String(url.absoluteString))
+                .onTapGesture {
+                    viewModel.loadPage(url: url)
+                    dismiss()
+                }
+        }
+        .navigationTitle("Bookmarks")
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Done") {
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+            .environment(WebViewModel())
+    }
 }
